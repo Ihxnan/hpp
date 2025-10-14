697deaf493cb6ad53d2c9c9377a479d8a5217f38
ihxnan <1377537238@qq.com>
2025-10-14 22:24:12 +0800

    update

--- headers/fenwick.cpp
+++ headers/fenwick.cpp
@@ -6,7 +6,7 @@
 template <class T> class Fenwick
 {
     std::vector<T> tree;
-    int n;
+    int length;
 
     T lowbit(int x)
     {
@@ -14,13 +14,13 @@ template <class T> class Fenwick
     }
 
   public:
-    Fenwick(int length = 100000) : tree(length + 1), n(length)
+    Fenwick(int length = 100000) : tree(length + 1), length(length)
     {
     }
 
     void add(int pos, T val)
     {
-        for (; pos <= n; pos += lowbit(pos))
+        for (; pos <= length; pos += lowbit(pos))
             tree[pos] += val;
     }
 
@@ -30,49 +30,11 @@ template <class T> class Fenwick
         for (; pos > 0; pos -= lowbit(pos))
             ans += tree[pos];
         return ans;
-    }#include <algorithm>
-#include <cmath>
-#include <cstring>
-#include <iostream>
-#include <map>
-#include <queue>
-#include <unordered_map>
-#include <vector>
+    }
 
-#define x first
-#define y second
-#define all(v) v.begin(), v.end()
-#define endl '\n'
-
-using namespace std;
-
-using ll = long long;
-using ull = unsigned long long;
-using vi = vector<int>;
-using vvi = vector<vi>;
-using vl = vector<ll>;
-using vvl = vector<vl>;
-using pii = pair<int, int>;
-using pll = pair<ll, ll>;
-using psi = pair<string, int>;
-
-
-const int iINF = 0x3f3f3f3f;
-const ll lINF = 0x3f3f3f3f3f3f3f3f;
-const int mod = 998244353;
-const ll MOD = 1000000007;
-
-ll gcd(ll a, ll b)
-{
-    return b ? gcd(b, a % b) : a;
-}
-
-template <class T, class... Rest> void print(const T& x, const Rest &...rest);
-}
-
-    T query(int start, int end) const
+    T querry(int left, int right) const
     {
-        return sum(end) - sum(start - 1);
+        return sum(right) - sum(left - 1); 
     }
 };
 
